8adc8a75cf2540d4846ddabf2bac16ebca57c51d
Veylar <veylar@mail.ru>
2018-12-10 19:51:55 +0300

    octopuses

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,13 +2,29 @@ import React from 'react'
 import SpinningLogo from 'components/SpinningLogo'
 import ClickCounter from '../ClickCounter/ClickCounter'
 
+const octopuses = [
+	{ isInversed: true },
+	{ isInversed: false },
+	{ isInversed: false },
+	{ isInversed: false },
+	{ isInversed: false },
+	{ isInversed: true },
+	{ isInversed: true },
+	{ isInversed: true },
+	{ isInversed: false },
+	{ isInversed: false },
+	{ isInversed: false },
+]
+
 export default class App extends React.Component {
 	render() {
 		return (
 			<div>
 				<h1 className="text-center">So here is some magic</h1>
 				<div className="row justify-center wrap">
-					<SpinningLogo />
+					{octopuses.map((octopus, i) => (
+						<SpinningLogo inverse={octopus.isInversed} key={i} />
+					))}
 				</div>
 				<ClickCounter />
 			</div>
