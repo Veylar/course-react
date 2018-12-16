cdad25ebd5ae31f23fffccb0649d793201f74a07
Veylar <veylar@mail.ru>
2018-12-16 14:22:36 +0300

    buttons 0.1

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,33 +1,51 @@
 import React from 'react'
 import SpinningLogo from 'components/SpinningLogo'
 import ClickCounter from '../ClickCounter/ClickCounter'
-
-const octopuses = [
-	{ isInversed: true },
-	{ isInversed: false },
-	{ isInversed: false },
-	{ isInversed: false },
-	{ isInversed: false },
-	{ isInversed: true },
-	{ isInversed: true },
-	{ isInversed: true },
-	{ isInversed: false },
-	{ isInversed: false },
-	{ isInversed: false },
-]
+import 'utils/creators'
+import Btn from 'components/Btn'
 
 export default class App extends React.Component {
-	render() {
-		return (
-			<div>
-				<h1 className="text-center">So here is some magic</h1>
-				<div className="row justify-center wrap">
-					{octopuses.map((octopus, i) => (
-						<SpinningLogo inverse={octopus.isInversed} key={i} />
-					))}
-				</div>
-				<ClickCounter />
-			</div>
-		)
-	}
+  constructor() {
+    super()
+    this.state = { octopuses: [this.createOctopus()] }
+  }
+
+  createOctopus = () => {
+    return { inInversed: true }
+  }
+
+  handleAdd = () => {
+    this.setState({ octopuses: [...this.state.octopuses, { isInversed: Math.round(Math.random) }] })
+  }
+
+  handleDelete = () => {
+    this.setState({
+      octopuses: this.state.octopuses.filter((octopus, i) => i !== this.state.octopuses.length - 1),
+    })
+  }
+
+  //   handleAllRight = () => {
+  //     this.setState({
+  //       octopuses: this.state.octopuses.map(octopus => (octopus, isInversed: true)))
+  //   }
+  // }
+
+  render() {
+    return (
+      <div>
+        <h1 className="text-center">So here is some magic</h1>
+        <div className="row justify-center wrap">
+          <Btn onClick={this.handleAdd}>Add</Btn>
+          <Btn onClick={this.handleDelete}>Delete</Btn>
+          <Btn onClick={this.handleAllRight}>All Right</Btn>
+        </div>
+        <div className="row justify-center wrap">
+          {this.state.octopuses.map((octopus, i) => (
+            <SpinningLogo inverse={octopus.isInversed} key={i} />
+          ))}
+        </div>
+        <ClickCounter />
+      </div>
+    )
+  }
 }
